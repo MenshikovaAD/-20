a1f7b5774a21746a66bdb3d91bec7d749ce91c90
Анастасия Меньшикова <menschickova2011@mail.ru>
2022-03-26 18:52:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_20
+{
+    internal class Program
+    {
+        delegate double MyDelegate(double R);    
+        static void Main(string[] args)
+        {
+            
+
+            MyDelegate myDelegate = Circumference;
+            myDelegate += AreaOfCircle;
+            myDelegate += BallVolume;
+            Console.WriteLine("Введите радиус круга R");
+            Double R = myDelegate(Convert.ToDouble(Console.ReadLine()));
+            
+
+            Console.ReadKey();
+        }
+        static double Circumference(double R)
+        {
+            double r = (double)2 * Math.PI * R;
+            Console.WriteLine("Длина окружности: {0}",  r);
+            return r;
+        }
+        
+        static double AreaOfCircle(double R)
+        {
+            double r = (double)Math.PI * R * R;
+            Console.WriteLine("Площадь круга: {0}", r);
+            return r;
+        }
+
+        static double BallVolume(double R)
+        {
+            double r = (double) (4 * Math.PI * R * R * R)/3;
+            Console.WriteLine("Объем шара: {0}", r);
+            return r;
+        }
+
+        
+    }
+}
